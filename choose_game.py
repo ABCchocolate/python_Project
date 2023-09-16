dc6e1505f2eb04fb3966553c4b12f91505c9f2b8
ABCchocolate <88300899+ABCchocolate@users.noreply.github.com>
2023-09-16 23:31:01 +0900

    Create choose_game.py
    숫자 맞추기 게임

---
+++ choose_game.py
@@ -0,0 +1,21 @@
+import random
+random_number = random.randint(1, 100)
+
+game_count = 1
+
+while True:
+    try:
+        my_number = int(input("1 ~ 100 사이의 숫자를 입력하세요: "))
+
+        if my_number > random_number:
+            print("down")
+        elif my_number < random_number:
+            print("up")
+        elif my_number == random_number:
+            print(f"축하합니다. {game_count}회 만에 맞췄습니다.")
+            break
+
+        game_count = game_count + 1
+
+    except:
+        print("에러가 발생했습니다. 숫자를 입력해주세요.")
